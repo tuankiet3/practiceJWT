79a61e493acb5a4fc75939a40c32a7b0078fb93c
tuan kiet <tnkiet0512@gmail.com>
2025-01-18 19:39:34 +0700

    helloWorld

--- 01-nodejs-javascript-starter/src/routes/api.js
+++ 01-nodejs-javascript-starter/src/routes/api.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const express = require("express");
 
 const routerAPI = express.Router();
 
@@ -7,12 +7,15 @@ const routerAPI = express.Router();
 
 // } = require('../controllers/apiController')
 
-
 // routerAPI.get('/users', getUsersAPI);
 // routerAPI.post('/users', postCreateUserAPI);
 // routerAPI.put('/users', putUpdateUserAPI);
 // routerAPI.delete('/users', deleteUserAPI);
 
-
+routerAPI.get("/", (req, res) => {
+  return res.status(200).json({
+    message: "Welcome to API",
+  });
+});
 
 module.exports = routerAPI; //export default
